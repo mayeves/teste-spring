024b0b7479295cccf560fe3797031273a438a40b
mayeves <mayeves@gmail.com>
2023-10-01 21:48:16 -0300

    commit

---
+++ travel-java-api/src/main/java/io/github/mayeves/traveljavaapi/TravelJavaApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.mayeves.traveljavaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelJavaApiApplication.class, args);
+	}
+
+}
